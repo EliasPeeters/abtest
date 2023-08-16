e2ffe49bf2a03d8cecfc0b8b48116a0884869094
Elias Peeters <Elias.peeters@me.com>
2023-08-16 11:25:00 +0200

    added sql logs

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'eliaspeeters.de',
+    user: 'testuser',
+    password: 'test1234',
+    database: 'database',
+    port: '3310',
+    charset: 'utf8mb4'
+})
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySQL connected');
+})
 
 app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    db.query('INSERT INTO logs (createdAt, message) VALUES (NOW(), "test")', (err, result) => {
+        res.send('Hello World');
+    })
 })
 
 app.listen(4444, () => {
